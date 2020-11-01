4903ce84e3b8638bffbaff5d86bf57c2085b2032
Raul <raulcote@hotmail.com>
2020-11-01 22:08:41 +0100

    Added some 'placeholder' stuff to PokemonProfile to don't forget about it's shape. Still todo.

--- src/pages/PokemonProfile.jsx
+++ src/pages/PokemonProfile.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import usePokemonFetcher from '../hooks/usePokemonFetcher';
 
-const PokemonProfile = () => <div>Pokemon Profile Page</div>;
+const PokemonProfile = () => {
+  const { name } = useParams();
+  const { errorFetching, isLoading, data: pokemon } = usePokemonFetcher(
+    `/pokemon/${name}`
+  );
+
+  if (errorFetching) {
+    return <div>Something went wrong...</div>;
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  // console.log('PokemonProfile Page :::: ', pokemon);
+
+  return (
+    <>
+      <p>ID: {pokemon.id}</p>
+      <p>
+        Type:{' '}
+        {pokemon.types.map(({ type }) => (
+          <li key={`${pokemon.id}-${type.name}`}>{type.name}</li>
+        ))}
+      </p>
+      <p>Height: {pokemon.height}</p>
+      <p>
+        Abilities:
+        {pokemon.abilities.map(({ ability }) => (
+          <li key={`${pokemon.id}-${ability.name}`}>{ability.name}</li>
+        ))}
+      </p>
+    </>
+  );
+};
 
 export default PokemonProfile;
