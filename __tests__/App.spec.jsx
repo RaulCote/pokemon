d2319bfd870e73367417d12269e742fe4191d124
Raul <raulcote@hotmail.com>
2020-11-02 23:35:19 +0100

    Fixed App.spec.jsx to don't call the pokemon endpoint.

--- __tests__/App.spec.jsx
+++ __tests__/App.spec.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../src/App';
 
+jest.mock('../src/services/pokemonService.js', () => ({
+  get: Promise.resolve({ data: [] }),
+}));
+
 describe('App', () => {
   it('should render without crashing', () => {
     render(<App />);
