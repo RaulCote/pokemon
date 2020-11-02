eafb355b6599748e278dab50ae615a3aad63cbaf
Raul <raulcote@hotmail.com>
2020-11-02 23:38:56 +0100

    Made some changes on Jest set up.

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/setUpTests.js'],
   moduleNameMapper: {
     '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(png|jpeg|jpg|gif)$': '<rootDir>/__mocks__/styleMock.js',
   },
 };
